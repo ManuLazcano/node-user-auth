5897de690a292b733892b331f2ea17adfa3e4c25
ManuLazcano <manu.lazcano17@gmail.com>
2024-10-14 17:24:06 -0300

    Agregar codificacion para el password

--- index.js
+++ index.js
@@ -6,27 +6,25 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo')
+  res.send('Hola mundo')
 })
 
 app.post('/login', (req, res) => {})
 app.post('/register', (req, res) => {
-    const { username, password } = req.body
-    console.log(req.body);
+  const { username, password } = req.body
+  console.log(req.body)
 
-    try {
-        const id = UserRepository.create({ username, password })
-        res.send({ id })
-    } catch (error) {
-        res.status(400).send(error.message)
-    }
-    
+  try {
+    const id = UserRepository.create({ username, password })
+    res.send({ id })
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
 })
 app.post('/logout', (req, res) => {})
 
-app.get ('/protected', (req, res) => [])
+app.get('/protected', (req, res) => [])
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    
+  console.log(`Server running on http://localhost:${PORT}`)
 })
