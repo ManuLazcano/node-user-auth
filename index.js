3e75a9e6cb0db5044728eeea1473b54bafd6d527
ManuLazcano <manu.lazcano17@gmail.com>
2024-10-14 15:53:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { PORT } from './config.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo')
+})
+
+app.post('/login', (req, res) => {})
+app.post('/register', (req, res) => {})
+app.post('/logout', (req, res) => {})
+
+app.get ('/protected', (req, res) => [])
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    
+})
