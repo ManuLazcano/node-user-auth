75cf7ec1c9f1a3feb397d083644c62df318d96ae
ManuLazcano <manu.lazcano17@gmail.com>
2024-10-14 16:19:19 -0300

    Agregar validaciones basicas

--- user-repository.js
+++ user-repository.js
@@ -9,6 +9,13 @@ const User = Schema('User', {
 })
 
 export class UserRepository {
-  static create ({ username, password }) {}
+  static create ({ username, password }) {
+    if (typeof username !== 'string') throw new Error('Username must be a string')
+    if (username.length < 3) throw new Error('Username must be at least 3 characteres long')
+
+    if (typeof password !== 'string') throw new Error('Password must be a string')
+    if (password.length < 6) throw new Error('Password must be at least 6 characteres long')
+  }
+
   static login ({ username, password }) {}
 }
